5ca258099573e7f3045e683624b70f12e6e74796
parth <parejiyaparth2368@gmail.com>
2023-01-08 01:02:17 +0530

    Logo Dark Light

--- src/App.js
+++ src/App.js
@@ -1,14 +1,46 @@
 import './App.css';
-import Navbar from './Navbar/Navbar';
-import Herobanner from './HeroBanner/Herobanner';
-import Footer from './Footer/Footer';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Navbar from './Layout/Navbar/Navbar';
+import Herobanner from './Components/HeroBanner/Herobanner';
+import Footer from './Layout/Footer/Footer';
+import Ourwork from '../src/Pages/Our Work Page/Ourwork'
+import Aboutus from './Pages/About Us/Aboutus';
+import Contactus from './Pages/Contact US/Contactus';
+import { useState } from 'react';
 
 function App() {
+
+  const [mode, setMode] = useState('dark')
+
+  const toggledarklight = () => {
+    if (mode === 'light') {
+      setMode('dark')
+      console.log(mode);
+    } else {
+      setMode('light')
+      console.log(mode);
+
+    }
+  }
+
   return (
     <div className="App">
-      <Navbar />
-      <Herobanner />
-      <Footer />
+      <div className={mode === 'dark' ? "dart-theme" : ""}>
+        <Router>
+          <Navbar mode={mode} toggledarklight={toggledarklight} />
+          <Routes>
+            <Route path='/' element={<Herobanner />} />
+            <Route path='/ourwork' element={<Ourwork />} />
+            <Route path='/aboutus' element={<Aboutus />} />
+            <Route path='/contactus' element={<Contactus />} />
+          </Routes>
+        </Router>
+        <Footer mode={mode} />
+      </div>
     </div>
   );
 }
---
+++ src/Components/HeroBanner/Herobanner.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './herobanner.css'
+
+const Herobanner = () => {
+    return (
+        <div className="herobanner px-5">
+            <div className="containe container-fluid">
+                <h1 className="banner_heading">The Future Of Technology Starts Here</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Herobanner
